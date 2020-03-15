b71b8484407fc40007d13ed1a11a8b8ca3eaf2ac
senyer <576085903@qq.com>
2020-03-15 22:57:18 +0800

    Add run dashboard

--- src/main/java/cloud/distkv/DistkvCloudApplication.java
+++ src/main/java/cloud/distkv/DistkvCloudApplication.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DistkvCloudApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(DistkvCloudApplication.class, args);
+    try {
+      SpringApplication.run(DistkvCloudApplication.class, args);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
   }
 
 }
--- src/main/java/cloud/distkv/api/RunnerApi.java
+++ src/main/java/cloud/distkv/api/RunnerApi.java
@@ -2,9 +2,12 @@ package cloud.distkv.api;
 
 import cloud.distkv.common.annotations.NoAuth;
 import cloud.distkv.common.response.BaseController;
+import cloud.distkv.distkvclient.DistkvCommandClient;
+import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Distkv dashboard.
@@ -13,8 +16,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/run")
 public class RunnerApi extends BaseController {
 
+  @Resource
+  private DistkvCommandClient distkvCommandClient;
+
   /**
    * Navigate to run dashboard.
+   *
    * @return run/run.html
    */
   @GetMapping()
@@ -22,5 +29,20 @@ public class RunnerApi extends BaseController {
   public String index() {
     return "run/run";
   }
+
+  /**
+   * Execute cmd.
+   *
+   * @param cmd cmd content
+   * @return result
+   */
+  @GetMapping("cmd")
+  @ResponseBody
+  @NoAuth
+  public String cmd(String cmd) {
+    //TODO (senyer) Add validate check. Make sure the format of each command is legal.
+    return distkvCommandClient.exec(cmd);
+  }
+
 
 }
