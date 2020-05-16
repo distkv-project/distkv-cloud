205af748cb50dba7b6926127d09435574bc54011
senyer <576085903@qq.com>
2020-05-16 20:32:59 +0800

    update gitignore

--- src/test/java/cloud/distkv/utils/JWTTest.java
+++ src/test/java/cloud/distkv/utils/JWTTest.java
@@ -1,6 +1,7 @@
 package cloud.distkv.utils;
 
 import cloud.distkv.common.utils.JwtUtil;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class JWTTest {
@@ -8,6 +9,7 @@ public class JWTTest {
   @Test
   public void testSign() {
     String tansen = JwtUtil.sign("tansen", "123456");
+    Assert.assertTrue(true);
     System.out.println(tansen);
   }
 }
