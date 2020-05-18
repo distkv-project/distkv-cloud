3e5d25b4a91cafd79214d0c7e31a0067912754a0
senyer <576085903@qq.com>
2020-05-18 09:11:24 +0800

    Fix test

--- src/test/java/cloud/distkv/utils/JWTTest.java
+++ src/test/java/cloud/distkv/utils/JWTTest.java
@@ -1,7 +1,6 @@
 package cloud.distkv.utils;
 
 import cloud.distkv.common.utils.JwtUtil;
-import org.junit.Assert;
 import org.junit.Test;
 
 public class JWTTest {
@@ -9,7 +8,6 @@ public class JWTTest {
   @Test
   public void testSign() {
     String tansen = JwtUtil.sign("tansen", "123456");
-    Assert.assertTrue(true);
     System.out.println(tansen);
   }
 }
